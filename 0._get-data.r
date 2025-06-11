ac3b6049ad2311413b22182a2c2d8271728821ee
JBGruber <johannesb.gruber@gmail.com>
2025-06-11 14:56:51 -0600

    update download script

--- 0._get-data.r
+++ 0._get-data.r
@@ -1,36 +1,45 @@
 # globus ls --long --format json c3e4dc38-a5e2-47d5-baa2-8b3f5b2a59db:/ > 0.datafiles.json
 
 library(tidyverse)
-data_files <- jsonlite::read_json(("0.datafiles.json")) |> 
-  pluck("DATA") |> 
-  bind_rows() |> 
+download_db_files <- function(urls, destfiles) {
+  destfiles_all <- destfiles
+  done <- file.exists(destfiles)
+  urls <- urls[!done]
+  destfiles <- destfiles[!done]
+  id <- seq_along(urls)
+  chunks <- split(id, ceiling(id / 30))
+  cli::cli_progress_step("downloading {sum(!done)} files")
+  for (chunk in cli::cli_progress_along(chunks)) {
+    curl::multi_download(
+      urls[chunk],
+      destfiles[chunk],
+      progress = FALSE
+    )
+    Sys.sleep(5)
+  }
+  cli::cli_progress_done()
+  done <- file.exists(destfiles)
+  if (any(!done)) {
+    cli::cli_alert_info("{sum(!done)} still missing")
+    download_db_files(urls, destfiles)
+  }
+  return(destfiles_all)
+}
+
+
+data_files <- jsonlite::read_json(("0.datafiles.json")) |>
+  pluck("DATA") |>
+  bind_rows() |>
   mutate(
     size_h = prettyunits::pretty_bytes(size),
     download_url = glue::glue("https://g-b81a79.a78b8.36fe.data.globus.org/{name}?download=1")
   )
 
-# get 15 random channels
+# download a sample for now
 set.seed(42)
-df <- data_files |> 
-  slice_sample(prop = 0.01) |> 
-  mutate(downloaded = curl::multi_download(
+df <- data_files |>
+  slice_sample(prop = 0.01) |>
+  mutate(downloaded = download_db_files(
     download_url,
     file.path("data", name)
   ))
-
-# library(dataverse)
-# Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")
-# 
-# data_files <- get_dataset(dataset = "https://doi.org/10.7910/DVN/1M5KHX") |> 
-#   pluck("files") |> 
-#   # head(15) |> 
-#   mutate(download_url = map_chr(id, \(id) get_file(id, return_url = TRUE)))
-# 
-# rio::export(data_files, "0._data_files.csv")
-# dir.create("data", showWarnings = FALSE)
-# 
-# curl::multi_download(
-#   urls = data_files$download_url[1:6], 
-#   destfile = file.path("data", data_files$filename[1:6])
-# )
-
